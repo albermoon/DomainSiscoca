fce9c63b37773da88269a95dca0a00975ce748ac
albermoon <albertoluna92@gmail.com>
2024-10-17 12:11:29 +0200

    add repository to study and medicalPassport

--- lib/repository/repository.dart
+++ lib/repository/repository.dart
@@ -1,3 +1,5 @@
 export 'medications/medication_respository.dart';
 export 'task/task_repository.dart';
 export 'task/task_response_repository.dart';
+export 'study/study_repository.dart';
+export 'passport/medical_passport_repository.dart';
