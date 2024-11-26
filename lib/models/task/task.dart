cd1a46e20ce4161c335a8efe09a7040fb92cd8bd
albermoon <albertoluna92@gmail.com>
2024-11-26 17:33:16 +0100

    modify to save, update task_survey

--- lib/models/task/task.dart
+++ lib/models/task/task.dart
@@ -3,30 +3,32 @@ import 'package:domain/domain.dart';
 class Task {
   final int? id;
   final String name;
-  final String advertisement;
-  final int assignedTasks;
-  final int createdById;
+  //show a alert message in the mobile
+  final String? advertisement;
+  final int? assignedTasks;
+  final int? createdById;
   final String description;
-  final TaskType taskType;
+  final TaskType? taskType;
   final int taskTypeId;
-  final String createdAt;
-  final String updatedAt;
+  //json with the survey
   final TaskDetails details;
-  final int status;
+  final String? createdAt;
+  final String? updatedAt;
+  final int? status;
 
   Task({
-    required this.advertisement,
-    required this.assignedTasks,
-    required this.createdAt,
-    required this.createdById,
-    required this.description,
-    this.id,
     required this.name,
-    required this.status,
-    required this.taskType,
-    required this.taskTypeId,
-    required this.updatedAt,
     required this.details,
+    required this.description,
+    this.advertisement,
+    this.assignedTasks,
+    this.createdById,
+    this.id,
+    this.status,
+    this.taskType,
+    required this.taskTypeId,
+    this.createdAt,
+    this.updatedAt,
   });
 
   Task copyWith({
@@ -75,13 +77,13 @@ class Task {
     }
 
     return Task(
+      id: json['id'] as int? ?? 0,
+      name: json['name'] as String? ?? '',
       advertisement: json['advertisement'] as String? ?? '',
       assignedTasks: json['assigned_tasks'] as int? ?? 0,
       createdAt: json['created_at'] as String? ?? '',
       createdById: json['created_by_id'] as int? ?? 0,
       description: json['description'] as String? ?? '',
-      id: json['id'] as int? ?? 0,
-      name: json['name'] as String? ?? '',
       status: json['status'] as int? ?? 0,
       taskType: taskType,
       taskTypeId: json['task_type_id'] as int? ?? 0,
@@ -90,19 +92,19 @@ class Task {
     );
   }
 
-  Map<String, Object> toJson() {
+   Map<String, dynamic> toJson() {
     return {
-      'advertisement': advertisement,
-      'assigned_tasks': assignedTasks,
-      'created_at': createdAt,
-      'created_by_id': createdById,
-      'description': description,
       if (id != null) 'id': id!,
       'name': name,
-      'status': status,
-      'task_type': taskType.toJson(),
+      if (advertisement != null) 'advertisement': advertisement!,
+      if (assignedTasks != null) 'assigned_tasks': assignedTasks!,
+      if (createdById != null) 'created_by_id': createdById!,
+      'description': description,
+      if (taskType != null) 'task_type': taskType!.toJson(),
       'task_type_id': taskTypeId,
-      'updated_at': updatedAt,
+      if (createdAt != null) 'created_at': createdAt!,
+      if (updatedAt != null) 'updated_at': updatedAt!,
+      if (status != null) 'status': status!,
       ...details.toJson(),
     };
   }
