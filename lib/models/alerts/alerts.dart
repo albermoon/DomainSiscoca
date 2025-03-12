d9d5a7c31cb2761984cdced2cacdaa24bfaf2416
albermoon <albertoluna92@gmail.com>
2025-03-12 15:29:32 +0100

    fix alert to new alert structure

--- lib/models/alerts/alerts.dart
+++ lib/models/alerts/alerts.dart
@@ -1,14 +1,14 @@
 import 'package:domain/models/models.dart';
 
 class Alert {
-  final String id;
+  final int id;
   final User patientId;
   final dynamic healthDataPoint;
   final AlertThreshold alertThreshold;
   final DateTime createdAt;
   final bool isRead;
   final DateTime? readAt;
-  final int? readByDoctorId;
+  final String? readByDoctorId;
 
   Alert({
     required this.id,
@@ -23,14 +23,14 @@ class Alert {
 
   factory Alert.fromJson(Map<String, dynamic> json) {
     return Alert(
-      id: json['id'] as String,
+      id: json['id'] as int,
       patientId: User.fromJson(json['patient']), 
       createdAt: DateTime.parse(json['created_at'] as String),
       isRead: json['is_read'] == true || json['is_read'] == 1,
       alertThreshold: AlertThreshold.fromJson(json['alert_threshold']),
       readAt: json['read_at'] != null ? DateTime.parse(json['read_at'] as String) : null,
       healthDataPoint: json['health_data_point'] as dynamic,
-      readByDoctorId: json['read_by_doctor_id'] as int?,
+      readByDoctorId: json['read_by_doctor_id'] as String?,
     );
   }
 
